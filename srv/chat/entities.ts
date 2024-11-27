78a9c2fd2f43a62faf6ca5b8a732481bddd09b3a
3749 <yuhang.xiao@hand-china.com>
2024-11-27 14:18:23 +0800

    Initial commit

---
+++ srv/chat/entities.ts
@@ -0,0 +1,15 @@
+export enum Sender {
+  AI = "AI",
+  HUMAN = "Human",
+  Assistant = 'assistant',
+  User = 'user'
+}
+
+// export interface IMessages {
+//     UUIDChat   : string;
+//     Comment : string;
+//     Reply : string;
+//     Sender : string;
+//     IsAdopted : Boolean;
+// }
+
